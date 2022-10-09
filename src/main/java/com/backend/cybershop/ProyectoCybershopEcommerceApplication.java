3f458833fad0b5bb26e9b9600af89378df1b1310
bryan <brayanby2002@gmail.com>
2022-10-08 23:21:22 -0500

    proyecto inicial

---
+++ src/main/java/com/backend/cybershop/ProyectoCybershopEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.cybershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCybershopEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCybershopEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/cybershop/ProyectoCybershopEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cybershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCybershopEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
